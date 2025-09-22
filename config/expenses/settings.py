7e79a97eb4c31d880b5f9dc75c59c0099ba7ad74
unknown <godisuneetha9381@gmail.com>
2025-09-22 14:48:03 +0530

    database

--- config/expenses/settings.py
+++ config/expenses/settings.py
@@ -66,9 +66,10 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
